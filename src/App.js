417a87b724dfbc5b02747bd6a91044dae21e3b2f
nina-small <nine-small@aliyun.com>
2022-03-23 12:31:38 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Comp from './test/Comp'
-const App = () => {
+import React from 'react'
+
+class Comp extends React.Component{
+  static defaultProps = {
+    a: 1,
+    b:2
+  }
+  render(){
+    return (
+      <h1>a:{this.props.a},b:{this.props.b}</h1>
+    )
+  }
+}
+
+
+
+export default function App() {
   return (
     <div>
       <Comp></Comp>
     </div>
-  );
+  )
 }
-
-export default App;
